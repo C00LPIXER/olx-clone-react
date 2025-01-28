91258b4a3dc1148b991183ebdcd3d4f139c4c2a9
c00LpXer <amalkrishanp@gmail.com>
2025-01-28 16:37:52 +0530

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -20,12 +20,12 @@ import { toast } from "react-toastify";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIguc22IQG29PfevQmqs6SIoOZD2GDwLo",
-  authDomain: "olx-clone-92d23.firebaseapp.com",
-  projectId: "olx-clone-92d23",
-  storageBucket: "olx-clone-92d23.firebasestorage.app",
-  messagingSenderId: "576972464059",
-  appId: "1:576972464059:web:2e6b4a51c75d6536fcae9e",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
